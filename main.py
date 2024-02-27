c8baa2845c2bd948f3e59a974bb52ba04e774569
OunissiDhiaEddine <dhia.ounissi@outlook.com>
2024-02-27 17:57:51 +0100

    updated arabic support

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from docx import Document
+import codecs
 
 ## this is the funciton for counting words ## 
 def count_words(file_path):
     try:
         if file_path.endswith('.txt'):
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r', encoding='utf-8') as file:
                 text = file.read()
                 word_count = len(text.split())
                 return word_count
         elif file_path.endswith('.docx'):
             doc = Document(file_path)
+            # Make sure to decode the text properly for Arabic
             word_count = sum(len(paragraph.text.split()) for paragraph in doc.paragraphs)
             return word_count
         else:
